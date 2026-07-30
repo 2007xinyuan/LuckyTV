2ca7710ec5ad719d1d610122f75faa0d06a49ad0
2007xinyuan-glitch <2007xinyuan@gmail.com>
2026-07-31 07:46:15 +0800

    fix: 添加缺失的Java源文件(JarLoader/JsLoader/SSLCompat/IPyLoader) + 修复包名引用 + 修复dec-0.1.2.jar扩展名

--- app/src/normal/java/com/github/catvod/crawler/pyLoader.java
+++ app/src/normal/java/com/github/catvod/crawler/pyLoader.java
@@ -1,11 +1,29 @@
 package com.github.catvod.crawler;
 
+import com.github.catvod.crawler.python.IPyLoader;
+
 import java.util.Map;
 
-public class pyLoader {
+public class pyLoader implements IPyLoader {
+    @Override
     public void clear() {
     }
-    public Object getSpider(String key, String cls, String ext) {
+
+    @Override
+    public void setConfig(String jsonStr) {
+    }
+
+    @Override
+    public void setRecentPyKey(String pyApi) {
+    }
+
+    @Override
+    public Spider getSpider(String key, String cls, String ext) {
+        return new SpiderNull();
+    }
+
+    @Override
+    public Object[] proxyInvoke(Map<String, String> params) {
         return null;
     }
 }
